ea0eb5a5a9ae1e9fca91310b4ebb4b691dd685e8
Mohammad <mohammad@Mohammads-MacBook-Air.local>
2024-10-05 16:27:46 +0330

    part-1

---
+++ app/(home)/page.js
@@ -0,0 +1,14 @@
+
+
+export default function Home() {
+  return (
+    <>
+      <div
+        className="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">
+        <h4 className="fw-bold">داشبورد</h4>
+      </div>
+
+      <div id="chartdiv"></div>
+    </>  
+    );
+}
